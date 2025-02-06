703da8cf63d02747da69274358f61a86a8e8a385
Jeferson Barrero <jebarrero14@gmail.com>
2025-02-05 21:15:36 -0500

    Add Tailwind CSS integration and global styles; update dependencies for improved styling and layout consistency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-import icon from "astro-icon";
 
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://dev.jeffercbs.tech/",
-  integrations: [sitemap(), tailwind(), icon(), mdx()],
+  integrations: [sitemap(), tailwindcss(), icon(), mdx()],
   redirects: {
     "/en/projects/[...slug]": "/projects/[...slug]",
     "/en/blog/p/[...slug]": "/blog/p/[...slug]",
