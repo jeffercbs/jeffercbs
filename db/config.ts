545a2b480851dc6c2ba58e37965a629cd464d988
Jeferson Barrero <jebarrero14@gmail.com>
2025-03-11 17:01:49 -0500

    Rename 'project' to 'affair' and add 'additionalInfo' field in database configuration

--- db/config.ts
+++ db/config.ts
@@ -3,20 +3,16 @@ import { defineDb, defineTable, column, NOW } from "astro:db";
 export const Client = defineTable({
   columns: {
     id: column.text({ primaryKey: true, unique: true }),
-    
+
     name: column.text(),
     email: column.text(),
     phone: column.text(),
     company: column.text(),
 
-    project: column.text(),
-    projectDescription: column.text(),
+    affair: column.text(),
     referenceLink: column.text(),
-    feactures: column.text(),
-    preferredTechnology: column.text(),
     estimatedBudget: column.text(),
-
-    specs: column.text({deprecated: true}),
+    additionalInfo: column.text(),
 
     createdAt: column.date({ default: NOW }),
   },
