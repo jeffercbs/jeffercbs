a2925b76f1015f9706bfecaaf338412c669f2a4f
Jeferson Barrero <jebarrero14@gmail.com>
2025-03-26 13:26:50 -0500

    Add function to filter top 10 topics based on tag frequency

--- src/utils/get-tags.ts
+++ src/utils/get-tags.ts
@@ -11,6 +11,18 @@ export function filterUniqueTopics(articles: articleType[]) {
   return Array.from(uniqueTags);
 }
 
+export function filterTop10Topics(articles: articleType[]) {
+  const tagCount = new Map<string, number>();
+
+  articles.forEach(({ data: { tags } }) => {
+    tags.forEach((tag) => {
+      tagCount.set(tag, (tagCount.get(tag) || 0) + 1);
+    });
+  });
+
+  return Array.from(tagCount.entries()).sort((a, b) => b[1] - a[1]);
+}
+
 export function filterArticlesByTags(topic: string, articles: articleType[]) {
   return articles.filter(({ data: { tags } }) => tags.includes(topic));
 }
